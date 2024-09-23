1d81a4069b6faa36539c5aa802ff74d5369270cf
NS NooR <non377938@gmail.com>
2024-09-23 22:49:47 +0600

    Node View Card

--- lib/views/screens/home/widgets/custom_node_card.dart
+++ lib/views/screens/home/widgets/custom_node_card.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/common_widgets/custom_text.dart';
 
-
 class CustomNodeCard extends StatelessWidget {
   final String tittle;
   final String description;
   final Color? bgColor;
   const CustomNodeCard({
-    super.key, required this.tittle, required this.description, this.bgColor,
+    super.key,
+    required this.tittle,
+    required this.description,
+    this.bgColor,
   });
 
   @override
@@ -23,8 +25,17 @@ class CustomNodeCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextWidget(tittle: tittle,tSize: 25,tColor: Colors.white,tWeight: FontWeight.w400,),
-              TextWidget(tittle: description,tColor: Colors.white,tSize: 17,)
+              TextWidget(
+                tittle: tittle,
+                tSize: 25,
+                tColor: Colors.white,
+                tWeight: FontWeight.w400,
+              ),
+              TextWidget(
+                tittle: description,
+                tColor: Colors.white,
+                tSize: 17,
+              )
             ],
           ),
         ),
