c02566800dc4abdcda26e66ad3f359895fb83113
NS NooR <non377938@gmail.com>
2024-09-29 21:49:29 +0600

    Note details page create

---
+++ lib/views/screens/note_details/note_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteDetails extends StatelessWidget {
+  const NoteDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
