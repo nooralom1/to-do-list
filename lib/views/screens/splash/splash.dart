688b2d398acb66a214e5430da188e1c854d5d897
NS NooR <non377938@gmail.com>
2024-09-30 00:13:17 +0600

    appBar

--- lib/views/screens/splash/splash.dart
+++ lib/views/screens/splash/splash.dart
@@ -6,7 +6,9 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Splash"),
+      ),
     );
   }
 }
